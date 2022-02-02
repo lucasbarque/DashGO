d24fde0d72931c73d96c3600557cf807cfdfa2e0
Lucas Barque da Silva <lucasbarquedasilva@hotmail.com>
2022-02-01 21:06:29 -0400

    Adição Provider Sidebar

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,12 +1,18 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Lucas Barque</Text>
-        <Text color="gray.300" fontSize="small">lucas.barque@gmail.com</Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Lucas Barque</Text>
+          <Text color="gray.300" fontSize="small">lucas.barque@gmail.com</Text>
+        </Box>
+      )}
       <Avatar size="md" name="Lucas Barque" src="https://github.com/lucasbarque.png" />
     </Flex>
   )
