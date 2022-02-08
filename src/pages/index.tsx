544aa439d4408226761129de97c06baf3b19d8a4
Lucas Barque da Silva <lucasbarquedasilva@hotmail.com>
2022-02-08 19:18:44 -0400

    Configuração Mirage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
 import { Flex, Button, Stack } from "@chakra-ui/react";
 import { SubmitHandler, useForm } from 'react-hook-form';
-import { Input } from '../components/Form/Input';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
+import { Input } from '../components/Form/Input';
+
 
 type SignInFormData = {
   email: string;
